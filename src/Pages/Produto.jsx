f16a5c43ed964e1b3332f00766305b79576c7c19
anaoli13jps <anaoli13jps@gmail.com>
2025-08-31 20:56:53 +0000

    modo desktop feito

--- src/Pages/Produto.jsx
+++ src/Pages/Produto.jsx
@@ -10,58 +10,157 @@ import setaesquerda from "../assets/setaesquerda.png"
 import setadireita from "../assets/setadireita.png"
 import setaEsquerdaEscuro from "../assets/setaEsquerdaEscuro.png"
 import setaDireitaEscuro from "../assets/setaDireitaEscuro.png"
+import React, { useState } from "react";
+
+const produtos = [
+    {
+      nome: "Filamento PLA Azul Claro",
+      descricao: "1,75mm",
+      preco: "R$87",
+      imagem: filamento,
+      corNome: "text-filamento",
+      corDescricao: "text-filamento2",
+    },
+    {
+      nome: "Display Lcd Tft Touchscreen",
+      descricao: "3.5 320 x 480",
+      preco: "R$194",
+      imagem: display,
+      corNome: "text-display",
+      corDescricao: "text-display2",
+    },
+    {
+      nome: "Micro Servo Motor (4 unid.)",
+      descricao: "9g Sg90",
+      preco: "R$52",
+      imagem: servo,
+      corNome: "text-servo",
+      corNomeEscuro: "dark:text-servo3",
+      corDescricaoEscuro: "dark:text-servo4",
+      corDescricao: "text-servo2"
+    },
+    {
+      nome: "Módulo Mp3 Dfplayer",
+      descricao: "Mini para arduino",
+      preco: "R$26",
+      imagem: modulo,
+      corNome: "text-modulo",
+      corDescricao: "text-modulo2",
+      corDescricaoEscuro: "dark:text-background",
+    },
+    {
+      nome: "Microfone Omnidirecional",
+      descricao: "Inmp441 precision l2c",
+      preco: "R$33",
+      imagem: microfone,
+      corNome: "text-microfone",
+      corNomeEscuro: "dark:text-background",
+      corDescricao: "text-microfone2"
+    },
+    {
+      nome: "Esp32",
+      descricao: "wifi bluetooth",
+      preco: "R$46",
+      imagem: esp32,
+      corNome: "text-microfone",
+      corNomeEscuro: "dark:text-background",
+      corDescricao: "text-esp322"
+    },
+    {
+      nome: "Projeto pronto",
+      descricao: "McBmo",
+      preco: "R$438",
+      imagem: projeto,
+      corNome: "text-filamento",
+      corDescricao: "text-servo"
+    }
+  ];
 
 export function Produto() {
+    const [indexAtivo, setIndexAtivo] = useState(0);
+    const prevProduto = () => {
+        setIndexAtivo((prev) => (prev === 0 ? produtos.length - 1 : prev - 1));
+    };
+    const nextProduto = () => {
+        setIndexAtivo((prev) => (prev === produtos.length - 1 ? 0 : prev + 1));
+    };
+    const produto = produtos[indexAtivo];
+
     return(
         <div className="bg-background dark:bg-background-header bg-cover min-h-screen">
             <Header />
-            <div className="flex">
-                <img src={setaesquerda} alt="seta esquerda" className="block dark:hidden"/>
-                <img src={setadireita} alt="seta direita" className="block dark:hidden"/>
-                <img src={setaEsquerdaEscuro} alt="seta esquerda escura" className="hidden dark:block"/>
-                <img src={setaDireitaEscuro} alt="seta direita escura" className="hidden dark:block"/>
-                <div>
-                    <img src={filamento} alt="filamento" />
-                    <p className="text-filamento">Filamento PLA Azul Claro</p>
-                    <p className="text-filamento2">1,75mm</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$87</p>
+            <div className="flex justify-center items-center gap-10 pt-30 font-inter px-10">
+
+                <div className="flex flex-col items-end gap-20">
+                    <div className="w-120">
+                        <h2 className={`text-4xl font-bold ${produto.corNome} ${produto.corNomeEscuro}`}>
+                            {produto.nome}
+                        </h2>
+                        <h2 className={`text-xl font-bold ${produto.corDescricao} ${produto.corDescricaoEscuro}`}>
+                            {produto.descricao}
+                        </h2>
+                    </div>
+                    <span className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo text-xl px-3 py-1">
+                        {produto.preco}
+                    </span>
                 </div>
-                <div>
-                    <img src={display} alt="display" />
-                    <p className="text-display">Display Lcd Tft Touchscreen</p>
-                    <p className="text-display2">3.5 320 x 480</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$194</p>
+
+                <div className="flex items-center">
+                    <img onClick={prevProduto} src={setaesquerda} alt="seta esquerda" className="block dark:hidden"/>
+                    <img onClick={prevProduto} src={setaEsquerdaEscuro} alt="seta esquerda escura" className="hidden dark:block"/>
+
+                    <img src={produto.imagem} alt={produto.nome} className="w-110 h-110" />
+
+                    <img onClick={nextProduto} src={setadireita} alt="seta direita" className="block dark:hidden"/>
+                    <img onClick={nextProduto} src={setaDireitaEscuro} alt="seta direita escura" className="hidden dark:block"/>
                 </div>
-                <div>
-                    <img src={servo} alt="servo" />
-                    <p className="text-servo dark:text-servo3">Micro Servo Motor (4 unid.)</p>
-                    <p className="text-servo2 dark:text-servo4">9g Sg90</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$52</p>
+                
+                {/* só pra garantir deixa ai a vergonha da burrice */}
+                <div className="hidden">
+                    <div>
+                        <img src={filamento} alt="filamento" />
+                        <p className="text-filamento">Filamento PLA Azul Claro</p>
+                        <p className="text-filamento2">1,75mm</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$87</p>
+                    </div>
+                    <div>
+                        <img src={display} alt="display" />
+                        <p className="text-display">Display Lcd Tft Touchscreen</p>
+                        <p className="text-display2">3.5 320 x 480</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$194</p>
+                    </div>
+                    <div>
+                        <img src={servo} alt="servo" />
+                        <p className="text-servo dark:text-servo3">Micro Servo Motor (4 unid.)</p>
+                        <p className="text-servo2 dark:text-servo4">9g Sg90</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$52</p>
+                    </div>
+                    <div>
+                        <img src={modulo} alt="modulo" />
+                        <p className="text-modulo">Módulo Mp3 Dfplayer</p>
+                        <p className="text-modulo2 dark:text-background">Mini para arduino</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$26</p>
+                    </div>
+                    <div>
+                        <img src={microfone} alt="microfone" />
+                        <p className="text-microfone dark:text-background">Microfone Omnidirecional</p>
+                        <p className="text-microfone2">Inmp441 precision l2c</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$33</p>
+                    </div>
+                    <div>
+                        <img src={esp32} alt="esp32" />
+                        <p className="text-microfone dark:text-background">Esp32</p>
+                        <p className="text-esp322">wifi bluetooth</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$46</p>
+                    </div>
+                    <div>
+                        <img src={projeto} alt="projeto" />
+                        <p className="text-filamento">Projeto pronto</p>
+                        <p className="text-servo">McBmo</p>
+                        <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$438</p>
+                    </div>
                 </div>
-                <div>
-                    <img src={modulo} alt="modulo" />
-                    <p className="text-modulo">Módulo Mp3 Dfplayer</p>
-                    <p className="text-modulo2 dark:text-background">Mini para arduino</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$26</p>
-                </div>
-                <div>
-                    <img src={microfone} alt="microfone" />
-                    <p className="text-microfone dark:text-background">Microfone Omnidirecional</p>
-                    <p className="text-microfone2">Inmp441 precision l2c</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$33</p>
-                </div>
-                <div>
-                    <img src={esp32} alt="esp32" />
-                    <p className="text-microfone dark:text-background">Esp32</p>
-                    <p className="text-esp322">wifi bluetooth</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$46</p>
-                </div>
-                <div>
-                    <img src={projeto} alt="projeto" />
-                    <p className="text-filamento">Projeto pronto</p>
-                    <p className="text-servo">McBmo</p>
-                    <p className="text-amarelo bg-dark-green dark:bg-filamento2 dark:text-servo">R$438</p>
-                </div>
+                
             </div>
         </div>
     )
--- src/Pages/SobreNos.jsx
+++ src/Pages/SobreNos.jsx
@@ -12,67 +12,61 @@ import decoracao from "../assets/decoracaoContato.png"
 import telefoneEscuro from "../assets/telefoneEscuro.png"
 import emailEscuro from "../assets/emailEscuro.png"
 
+const images = [
+    maria,
+    igor,
+    julia,
+    pedro,
+    ana,
+  ];
+
 export function SobreNos() {
     return(
         <div className="bg-background dark:bg-background-header bg-cover min-h-screen">
             <Header />
-            <div className="flex flex-col gap-30">
-                <div className="flex">
-                    <div>
-                        <img src={maria} alt="Maria" />
-                        <p className="text-green-button">Maria Clara Alves</p>
-                        <p className="text-background">@mahx_013</p>
-                    </div>
-                    <div>
-                        <img src={igor} alt="Igor" />
-                        <p className="text-green-button">Igor Andrade</p>
-                        <p className="text-background">@ingufck</p>
-                    </div>
-                    <div>
-                        <img src={julia} alt="Julia" />
-                        <p className="text-green-button">Julia Maciel</p>
-                        <p className="text-background">@juliaz.x</p>
-                    </div>
-                    <div>
-                        <img src={pedro} alt="Pedro" />
-                        <p className="text-green-button">Pedro Moura</p>
-                        <p className="text-background">@mourapms15</p>
-                    </div>
-                    <div>
-                        <img src={ana} alt="Ana" />
-                        <p className="text-green-button">Ana Julia Oliveira</p>
-                        <p className="text-background">@anajulia.o12</p>
-                    </div>
-                </div>
-                <div className="flex justify-center gap-30">            
-                    <div className="flex justify-center bg-gray-input px-10 py-15 w-fit rounded-sm">
-                        <div className="bg-background flex flex-col  w-120 gap-5 p-10 rounded-sm">
-                            <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm" placeholder="Nome"></textarea>
-                            <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm" placeholder="Email"></textarea>
-                            <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm h-50" placeholder="Mensagem"></textarea>
-                            <button className="bg-background-header w-40 p-2 text-lg cursor-pointer text-background rounded-sm">Enviar mensagem</button>
-                        </div>
+            <div className="flex flex-col">
+                <div className="flex flex-col gap-30">
+                    <div className="flex pt-20 gap-10 justify-center">
+                        {images.map((src, index) => (
+                            <div key={index} className="relative overflow-hidden rounded-2xl cursor-pointer transition-all duration-300 ease-in-out">
+                            <img src={src} alt={`imagem-${index}`} className="object-cover rounded-xl h-120 w-30 hover:w-100 transition-all duration-300 ease-in-out hover:border-2 hover:border-yellow-500"/>
+                            </div>
+                        ))}
                     </div>
 
-                    <div className="flex flex-col items-center gap-20">
-                        <h2 className="text-5xl text-background-header dark:text-background">ENTRE EM CONTATO</h2>
-                        <div className="flex gap-10">
-                            <div className="flex flex-col justify-center items-center gap-10">
-                                <img className="w-10 block dark:hidden" src={telefone} alt="telefone" />
-                                <img className="w-10 hidden dark:block" src={telefoneEscuro} alt="telefone escuro" />
-                                <p className="text-background-header dark:text-green-contato">(11) 97076-5240</p>
+                    <div className="flex justify-center gap-30">            
+                        <div className="flex justify-center bg-gray-input px-10 py-15 w-fit rounded-sm">
+                            <div className="bg-background flex flex-col  w-120 gap-5 p-10 rounded-sm">
+                                <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm" placeholder="Nome"></textarea>
+                                <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm" placeholder="Email"></textarea>
+                                <textarea className="border bg-green-input w-100 pl-2 text-background-header rounded-sm h-50" placeholder="Mensagem"></textarea>
+                                <button className="bg-background-header w-40 p-2 text-lg cursor-pointer text-background rounded-sm">Enviar mensagem</button>
                             </div>
-                            <img className="block dark:hidden" src={linha} alt="" />
-                            <img className="hidden dark:block" src={linhaEscura} alt="" />
-                            <div className="flex flex-col justify-center items-center gap-10">
-                                <img className="w-10 block dark:hidden" src={email} alt="email" />
-                                <img className="w-10 hidden dark:block" src={emailEscuro} alt="email escuro" />
-                                <p className="text-background-header dark:text-green-contato">mc.bmo@eaportal.org</p>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-20">
+                            <h2 className="text-5xl text-background-header dark:text-background">ENTRE EM CONTATO</h2>
+                            <div className="flex gap-10">
+                                <div className="flex flex-col justify-center items-center gap-10">
+                                    <img className="w-10 block dark:hidden" src={telefone} alt="telefone" />
+                                    <img className="w-10 hidden dark:block" src={telefoneEscuro} alt="telefone escuro" />
+                                    <p className="text-background-header dark:text-green-contato">(11) 97076-5240</p>
+                                </div>
+                                <img className="block dark:hidden" src={linha} alt="" />
+                                <img className="hidden dark:block" src={linhaEscura} alt="" />
+                                <div className="flex flex-col justify-center items-center gap-10">
+                                    <img className="w-10 block dark:hidden" src={email} alt="email" />
+                                    <img className="w-10 hidden dark:block" src={emailEscuro} alt="email escuro" />
+                                    <p className="text-background-header dark:text-green-contato">mc.bmo@eaportal.org</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <img className="w-300" src={decoracao} alt="" />
+                
+                <div className="flex justify-end">
+                    <img className="w-200" src={decoracao} alt="" />
+                </div>
             </div>
         </div>
     )
